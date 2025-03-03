1e310a634338b455e977b1f86eff5cc8c1747d7d
xblmati <mraczynski01@gmail.com>
2025-03-03 10:18:12 +0100

    Added features 9 and 10

--- ConsoleApp1/StringCalculator.cs
+++ ConsoleApp1/StringCalculator.cs
@@ -4,13 +4,22 @@ public class StringCalculator
 {
     public int Calculate(string arg)
     {
-        var delimiters = new List<char> { ',', '\n' };
+        var delimiters = new List<string> { ",", "\n" };
         if (arg.StartsWith("//"))
         {
-            delimiters.Add(arg[2]);
-            arg = arg[3..];
+            var indexOfNewLine = arg.IndexOf('\n');
+            var delimiterString = arg[2..indexOfNewLine];
+            if (delimiterString.Length == 1)
+                delimiters.Add(delimiterString);
+            else
+            {
+                if (delimiterString[0] != '[' || delimiterString[^1] != ']') throw new FormatException();
+                var delimiterSplit = delimiterString[1..^1].Split("][");
+                delimiters.AddRange(delimiterSplit);
+            }
+            arg = arg[(indexOfNewLine + 1)..];
         }
-        var split = arg.Split(delimiters.ToArray());
+        var split = arg.Split(delimiters.ToArray(), StringSplitOptions.None);
         return split.Sum(str => int.TryParse(str, out var result)
             ? result switch
             {
